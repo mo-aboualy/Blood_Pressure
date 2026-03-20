44f66e9d18c4998741483470cf102ca04151fcf8
Mohamed  Aboualy <mohamedaboualy96@gmail.com>
2026-03-20 14:19:10 +0200

    Add blood pressure classification script

---
+++ blood_pressure.m
@@ -0,0 +1,29 @@
+clc
+clear
+n=input('Enter the number of the patients : '); %mohamed ahmed
+HBP_counter=0;
+PHBP_counter=0;
+IBP_counter=0;
+LBP_counter=0;
+for i=1:n
+    fprintf('Enter the blood pressure for patient %d\n',i);
+    x(i)=input('');
+end
+
+for j=1:n
+    if x(j)>=140
+        HBP_counter=HBP_counter+1;
+    elseif x(j)>=120 && x(j)<140
+        PHBP_counter=PHBP_counter+1;
+    elseif x(j)>=90 && x(j)<120
+        IBP_counter=IBP_counter+1;
+    elseif x(j)>=70 && x(j)<90
+        LBP_counter=LBP_counter+1;
+    end
+end
+fprintf('Number of patients with HBP : %d\n',HBP_counter);
+fprintf('Number of patients with PHBP : %d\n',PHBP_counter);
+fprintf('Number of patients with IBP : %d\n',IBP_counter);
+fprintf('Number of patients with LBP : %d\n',LBP_counter);
+
+output_array=[HBP_counter,PHBP_counter,IBP_counter,LBP_counter]
